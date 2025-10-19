dd6ccaa698ae925d7c4a6b0ef5aef40a5910e296
Rajab Natshah <rajabn@gmail.com>
2025-10-19 20:17:57 +0300

    [#3553260] chore(tooling): Update Node.js to >=20.0 and Yarn to 4.9.3 for Varbase Total Control Dashboard module to align with Drupal Core

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,8 @@ function compile () {
     indentType: 'space',
     indentWidth: 2,
     linefeed: 'lf',
-    sourceMap: false
+    sourceMap: false,
+    silenceDeprecations: ['legacy-js-api', 'import', 'global-builtin']
   };
 
   return gulp.src([paths.scss.src])
